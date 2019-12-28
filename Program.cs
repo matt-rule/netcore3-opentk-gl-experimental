6f7e09869ea96657bac702ad6a558d32238561d4
matt-rule <matt.rule.github@gmail.com>
2019-12-28 20:00:36 +0000

    Make a window appear

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using OpenTK;
 
 namespace netcore3_opentk_gl_experimental
 {
@@ -6,7 +7,7 @@ namespace netcore3_opentk_gl_experimental
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new MainWindow().Run(60);
         }
     }
 }

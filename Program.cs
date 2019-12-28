2b7d03ba8b1876ac212233739c8bfb454bc0e71a
Matt Rule <matt.rule.github@gmail.com>
2019-12-28 18:38:05 +0000

    Dotnet standard project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netcore3_opentk_gl_experimental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

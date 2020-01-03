5a1a20d219dda68dd8ff89f1eb2f91a6e94089be
matt-rule <matt.rule.github@gmail.com>
2020-01-03 20:28:35 +0000

    Added submodule

--- Program.cs
+++ Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using netcore3_simple_game_engine;
 using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
 
 namespace netcore3_opentk_gl_experimental
 {
     class Program
     {
+        private const int DEFAULT_WIDTH = 750;
+        private const int DEFAULT_HEIGHT = 500;
+        public const int MAJOR_VERSION = 4;
+        public const int MINOR_VERSION = 5;
+        private const String WINDOW_TITLE = "OpenTK Demo";
         public const int FRAMES_PER_SECOND = 60;
+        Scene scene;
+
+        Program()
+        {
+            var sceneInfo = new SceneInfo {
+                Width = DEFAULT_WIDTH,
+                Height = DEFAULT_HEIGHT
+            };
+            scene = new Scene(sceneInfo);
+        }
+
+        private static void OnResize(EventArgs e)
+        {
+            
+        }
+
+        private static void OnLoad(EventArgs e)
+        {
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
+            GL.PatchParameter(PatchParameterInt.PatchVertices, 3);
+
+            GL.Enable(EnableCap.Blend);
+            GL.BlendEquation(BlendEquationMode.FuncAdd);
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+        }
 
         static void Main(string[] args)
         {
-            new MainWindow().Run(FRAMES_PER_SECOND);
+            WindowOptions options = new WindowOptions {
+
+            };
+
+            new MainWindow(options).Run(FRAMES_PER_SECOND);
         }
     }
 }
---
+++ Scene.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using System.Linq;
+using netcore3_simple_game_engine;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+
+namespace netcore3_opentk_gl_experimental
+{
+    public struct SceneInfo
+    {
+        public int Width;
+        public int Height;
+    };
+
+    public class Scene : IScene
+    {
+        private SceneInfo sceneInfo;
+        private double RotationAngle = 0.0;
+
+        public List<Object3d> GameObjects = null;
+        private ShaderProgram MainShaderProgram;
+
+        public Scene(SceneInfo info)
+        {
+            sceneInfo = info;
+            
+            GameObjects = new List<BufferData> {
+                Geometry.GetTriangle1(),
+                Geometry.GetTriangle2()
+            }
+            .Select(x => GlCustomUtil.GeometryTo3dObject(x))
+            .ToList();
+        }
+
+        // Usage: Call after OpenGL has been initialised.
+        public void Intialise()
+        {
+            // TODO: Use the unbind function to clean up. Maybe have a class CustomGlContext.
+            foreach (Object3d obj3d in GameObjects)
+                GlCustomUtil.Bind3dObject(obj3d);
+
+            MainShaderProgram = GlCustomUtil.CreateShaderProgram();
+        }
+
+        public void Update(double elapsedTime)
+        {
+            RotationAngle += elapsedTime;
+
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Matrix4 projMatrix = Matrix4.CreateOrthographicOffCenter(0, sceneInfo.Width, 0, sceneInfo.Height, -1.0f, 1.0f);
+            Matrix4 viewMatrix = Matrix4.Identity;
+            Matrix4.CreateRotationZ((float)RotationAngle, out Matrix4 tempMatrix);
+            Matrix4.CreateTranslation(-50.0f, -50.0f, 0.0f, out Matrix4 tempMatrix2);
+
+            foreach (var obj in GameObjects)
+            {
+                Matrix4 mvp = tempMatrix2 * tempMatrix * obj.ModelMatrix * viewMatrix * projMatrix;
+                    
+                GL.UseProgram(MainShaderProgram.ProgramId);
+
+                GlCustomUtil.Bind3dObject(obj);
+
+                GL.UniformMatrix4(MainShaderProgram.MatrixShaderLocation, false, ref mvp);
+
+                GL.DrawElements(PrimitiveType.Triangles, obj.Indices.Length, DrawElementsType.UnsignedInt, 0);
+                var x = GL.GetError();
+            }
+        }
+    }
+}
